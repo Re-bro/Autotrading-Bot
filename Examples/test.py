68e4fb5fcda03b45685adf0c481d56744a8a6e8d
Rebro <pjh6792@gmail.com>
2021-05-14 21:10:58 +0900

    Create test.py

---
+++ Examples/test.py
@@ -0,0 +1,11 @@
+import pyupbit
+import time
+import datetime
+
+# 'interval'초 마다 비트코인 가격 가져오기
+interval = 1
+while True:
+    now = str(datetime.datetime.now()).split('.')
+    cur_price = pyupbit.get_current_price("KRW-BTC")
+    print(now[0], "현재가 :", cur_price)
+    time.sleep(interval)
