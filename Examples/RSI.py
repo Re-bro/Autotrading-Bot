8ff12be5db5e4ed45806fce91e54e10cc5334ade
Rebro <pjh6792@gmail.com>
2021-05-17 15:24:08 +0900

    RSI 계산 최종

--- Examples/RSI.py
+++ Examples/RSI.py
@@ -1,24 +1,27 @@
 # RSI 지표 계산하는 코드 
 # ticker에 원하는 암호화폐, interval에 원하는 분/일/시
+# 매 초마다 현재 RSI값 출력
 
 import pyupbit
 import pandas
+import datetime
 import time
 
 def rsi(ohlc: pandas.DataFrame, period: int = 14):
     delta = ohlc["close"].diff()
-    gains, declines = delta.copy(), delta.copy()
-    gains[gains<0] = 0
-    declines[declines > 0] = 0
+    ups, downs = delta.copy(), delta.copy()
+    ups[ups < 0] = 0
+    downs[downs > 0] = 0
 
-    _gain = gains.ewm(com=(period - 1), min_periods=period).mean()
-    _loss = declines.abs().ewm(com=(period-1), min_periods=period).mean()
+    AU = ups.ewm(com = period-1, min_periods = period).mean()
+    AD = downs.abs().ewm(com = period-1, min_periods = period).mean()
+    RS = AU/AD
 
-    RS = _gain/_loss
-    return pandas.Series(100 - (100/(1+RS)), name = "RSI")
+    return pandas.Series(100 - (100/(1 + RS)), name = "RSI")  
+
 
 while True:
     data = pyupbit.get_ohlcv(ticker="KRW-XRP", interval="minute5")
-    nrsi = rsi(data, 14).iloc[-1]
-    print(nrsi)
-    time.sleep(0.5)
+    now_rsi = rsi(data, 14).iloc[-1]
+    print(datetime.datetime.now(), now_rsi)
+    time.sleep(1)
