0c5e0221c18b2854699771f89bd7c347de60adc1
Rebro <pjh6792@gmail.com>
2021-05-27 20:47:02 +0900

    매매 비율 수정

--- BOT/Trade.py
+++ BOT/Trade.py
@@ -7,14 +7,15 @@ from apikey import upbit
 # 시장가 매수 함수
 def buy(coin):
     money = upbit.get_balance("KRW")
-    if money < 20000 :
+    if money < 15000 :
         res = upbit.buy_market_order(coin, money)
-    elif money < 50000:
-        res = upbit.buy_market_order(coin, money*0.4)
+    elif money < 50000 :
+        res = upbit.buy_market_order(coin, 10000)
     elif money < 100000 :
-        res = upbit.buy_market_order(coin, money*0.3)
+        res = upbit.buy_market_order(coin, 15000)
     else :
-        res = upbit.buy_market_order(coin, money*0.2)
+        res = upbit.buy_market_order(coin, money*0.15)
+    print(res)
     return
 
 # 시장가 매도 함수
@@ -22,12 +23,15 @@ def sell(coin):
     amount = upbit.get_balance(coin)
     cur_price = pyupbit.get_current_price(coin)
     total = amount * cur_price
-    if total < 20000 :
+    if total < 15000 :
         res = upbit.sell_market_order(coin, amount)
-    elif total < 50000:
-        res = upbit.sell_market_order(coin, amount*0.4)
-    elif total < 100000:
-        res = upbit.sell_market_order(coin, amount*0.3)        
+    elif total < 30000 :
+        res = upbit.sell_market_order(coin, amount*0.5)
+    elif total < 50000 :
+        res = upbit.sell_market_order(coin, amount*0.35)
+    elif total < 100000 :
+        res = upbit.sell_market_order(coin, amount*0.2)        
     else :
-        res = upbit.sell_market_order(coin, amount*0.2)
+        res = upbit.sell_market_order(coin, amount*0.15)
+    print(res)
     return
--- BOT/main.py
+++ BOT/main.py
@@ -4,35 +4,32 @@ import datetime
 import time
 from Trade import buy, sell
 from RSI import rsi
+from apikey import upbit
 
 # 이용할 코인 리스트
-coinlist = ["KRW-BTC", "KRW-XRP", "KRW-ETC", "KRW-ETH", "KRW-BCH", "KRW-EOS"] # Coin ticker 추가
+coinlist = ["KRW-ETC", "KRW-BCH", "KRW-EOS"] # Coin ticker 추가
 lower28 = []
-higher70 = []
-
+higher72 = []
 # initiate
 for i in range(len(coinlist)):
     lower28.append(False)
-    higher70.append(False)
+    higher72.append(False)
 
 # RSI에 따른 매매 작업
 while(True): # 사용시 True로
     for i in range(len(coinlist)):
         data = pyupbit.get_ohlcv(ticker=coinlist[i], interval="minute3")
         now_rsi = rsi(data, 14).iloc[-1]
-        print("코인명: ", coinlist[i])
-        print("현재시간: ", datetime.datetime.now())
-        print("RSI :", now_rsi)
-        print()
-        if now_rsi <= 28 : 
+        if now_rsi <= 28 and lower28[i] == False: 
             lower28[i] = True
-        elif now_rsi >= 33 and lower28[i] == True:
+            print("코인명: ", coinlist[i])
             buy(coinlist[i])
-            lower28[i] = False
-        elif now_rsi >= 70 and higher70[i] == False:
+        elif now_rsi >= 72 and higher72[i] == False:
+            print("코인명: ", coinlist[i])
             sell(coinlist[i])
-            higher70[i] = True
-        elif now_rsi <= 60 :
-            higher70[i] = False
+            higher72[i] = True
+        elif now_rsi >= 45 and now_rsi <= 55:
+            lower28[i] = False
+            higher72[i] = False
     time.sleep(0.5)
 
